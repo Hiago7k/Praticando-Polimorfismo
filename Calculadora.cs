b527f920354874ba47b7cab23b8d78fb050e80db
Hiago 7k <hiago7k@gmail.com>
2026-03-11 20:38:03 -0300

    utilizando polimorfismo

--- Calculadora.cs
+++ Calculadora.cs
@@ -9,4 +9,11 @@ internal class Calculadora
         var resposta = numero1 + numero2;
         Console.WriteLine(resposta);
     }
+
+
+    public void Somar(int numero1, int numero2, int numero3)
+    {
+        var resposta = numero1 + numero2 + numero3;
+        Console.WriteLine(resposta);
+    }
 }
