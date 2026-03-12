8389234051c39533ab5cd24a2dcf829b062c41af
Hiago 7k <hiago7k@gmail.com>
2026-03-12 20:39:18 -0300

    usando heranca no ctor

--- Imagem.cs
+++ Imagem.cs
@@ -2,8 +2,13 @@
 
 namespace Praticando_Polimorfismo;
 
-internal class Imagem
+internal class Imagem : Midia
 {
+    public Imagem(string resolucao, string nome)
+      :base(nome)
+    {
+        Resolucao = resolucao;
+    }
 
     public string Resolucao { get; set; }
 }
--- Video.cs
+++ Video.cs
@@ -1,7 +1,13 @@
 ﻿
 namespace Praticando_Polimorfismo;
 
-internal class Video
+internal class Video : Midia
 {
+    public Video(double duracao, string nome)
+        :base(nome)
+    {
+        Duracao = duracao;
+    }
+
     public double Duracao { get; set; }
 }
