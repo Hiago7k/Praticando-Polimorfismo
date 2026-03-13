15a550f809ccbbfe77a413384d6a67021c065dc1
Hiago 7k <hiago7k@gmail.com>
2026-03-12 21:09:14 -0300

    criando classe reserva online com metodo

---
+++ ReservaOnline.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Praticando_Polimorfismo;
+
+internal class ReservaOnline : Reserva
+{
+
+
+    public override void Confirmar()
+    {
+        Console.WriteLine("Reserva online");
+    }
+}
--- ReservaPresencial.cs
+++ ReservaPresencial.cs
@@ -6,7 +6,6 @@ internal class ReservaPresencial : Reserva
 
     public override void Confirmar()
     {
-        Console.WriteLine("Confirmando presencial");
-        
+        Console.WriteLine("Confirmando presencial");   
     }
 }
