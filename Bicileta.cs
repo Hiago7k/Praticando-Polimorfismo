3ce52a9d957122d98db5a6fe14ca3c0c16b159cf
Hiago 7k <hiago7k@gmail.com>
2026-03-16 18:47:59 -0300

    criando bicileta

---
+++ Bicileta.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Praticando_Polimorfismo;
+
+internal class Bicileta : Transporte
+{
+
+    public override void CalcularTempo(int distanciaKm)
+    {
+        var tempoEstimado = (distanciaKm * 4);
+
+    }
+}
