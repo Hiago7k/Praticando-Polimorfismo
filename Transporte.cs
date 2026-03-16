9330a8adbcd4f1242eb906f301de8e6813994df5
Hiago 7k <hiago7k@gmail.com>
2026-03-16 18:42:25 -0300

    criando metodo que vai ser sobrescrito

--- Transporte.cs
+++ Transporte.cs
@@ -3,4 +3,10 @@ namespace Praticando_Polimorfismo;
 
 internal class Transporte
 {
+
+    public virtual void CalcularTempo(int distanciaKm)
+    {
+        Console.WriteLine($"Retorna o valor em minutos");
+        Console.WriteLine($"");
+    }
 }
