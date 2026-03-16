f57f1590c223b171a859976e39db8da1a3c77b17
Hiago 7k <hiago7k@gmail.com>
2026-03-16 18:47:05 -0300

    criando metro

---
+++ Metro.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Praticando_Polimorfismo;
+
+internal class Metro : Transporte
+{
+
+    public override void CalcularTempo(int distanciaKm)
+    {
+        var tempoEstimado = (distanciaKm + 5) ;
+
+    }
+}
--- Onibus.cs
+++ Onibus.cs
@@ -1,5 +1,10 @@
 ﻿namespace Praticando_Polimorfismo;
 
-internal class Onibus
+internal class Onibus : Transporte
 {
+
+    public override void CalcularTempo(int distanciaKm)
+    {
+        var tempoEstimado = (distanciaKm * 2) + 5;
+    }
 }
