eb40c34d6353d7e08e08d83bfb6b22c02ce020cb
Hiago 7k <hiago7k@gmail.com>
2026-03-16 18:32:31 -0300

    criando outra classe, que vai fazer parte da nossa lista dinamica

---
+++ MaterialComplementar.cs
@@ -0,0 +1,12 @@
+﻿namespace Praticando_Polimorfismo;
+
+internal class MaterialComplementar : Conteudo
+{
+
+    public override void ExibirInfo()
+    {
+        Console.WriteLine($"Titulo {titulo}");
+        Console.WriteLine($"Paginas: {Paginas}");
+
+    }
+}
