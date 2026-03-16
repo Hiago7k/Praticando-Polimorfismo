5e260bdf68909b249c3f6ef5b7c85ce42ff1f3b4
Hiago 7k <hiago7k@gmail.com>
2026-03-16 19:43:08 -0300

    criando classe para excluir

--- EditarAcao .cs
+++ EditarAcao .cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Praticando_Polimorfismo;
 
-namespace Praticando_Polimorfismo
+internal class EditarAcao : IAcaoBotao
 {
-    internal class EditarAcao
+
+    public void Executar()
     {
+        Console.WriteLine("Editando informacoes de cadastro...");
     }
 }
---
+++ ExcluirAcao.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Praticando_Polimorfismo;
+
+internal class ExcluirAcao : IAcaoBotao
+{
+
+    public void Executar()
+    {
+        Console.WriteLine("Excluindo registros do sistema...");
+    }
+}
